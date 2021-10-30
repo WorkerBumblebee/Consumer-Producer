d439e102c0b8f26b7f9f6bd42a25b8dd8114d4d7
WorkerBumblebee <89601706+WorkerBumblebee@users.noreply.github.com>
2021-10-30 09:17:05 -0400

    Update producer.c

--- producer.c
+++ producer.c
@@ -12,7 +12,9 @@ int main()
 {
         // Allocated shared memory and semaphore
 
-                //Open am
+                //Open a block of memory named "table",
+                //created if it did not already exist,
+                //with all bits able to be read and written
         int shm_fd = shm_open("table", O_CREAT, 0666);
  
                 //Create a mapping of a virtual adress space,
@@ -31,9 +33,9 @@ int main()
                 //all bits able to be read or written
                 //and initialized with the intial value
 
-                         //fill - indicates the current number of elements
+                        //fill - indicates the current number of elements
                         //in the map, starts off as empty (0)
-       	sem_t* fill = sem_open("fill", O_CREAT, 1666, 1);
+       	sem_t *fill = sem_open("fill", O_CREAT, 1666, 1);
 
                         //avail - indicates the number of spots remaning
                         //in the map, starts off as all (2)
@@ -54,50 +56,89 @@ int main()
         // which will cause it to wait if there are
         // no items in the shared memory.
 
-/*      int loop = 20;
-        printf("\nProducer ready to create %d items.\n", loop);
-printf("\nBeforep\n");
+ //Number of elements the producer has to produce (20)
 
-//      for(int i = 1; i < loop; ++i) {
-//printf("\nInp\n");
-//              sem_wait(avail);
-//printf("ssdasd");
-/*
-               int ms = rand() % 2 + 1;
+        int loop = 20;
+                
+                //Print state that indicates the number of elements the producer
+                //will produce (in total) 
+ 
+        printf("\nProducer ready to create %d items.\n", loop);
+ 
+//Debugging - printf("\nBefore\n");
+ 
+                //Produce for 20 times
+        for(int i = 1; i < loop; ++i) {
+         
+//Debugging - printf("\nIn\n");
+         
+                        //Producer must wait if semaphore is full (nowhere
+                        //to produce) - i.e. avail = 0
+                        //function will decrement the semaphore's value
+                        //only if that will result in a nonnegtive value
+                        //else it waits for avail to be incremented to
+                        //satisfy the condition (wait == --)
+              sem_wait(avail);
+         
+//Deubugging - printf("Unpausing");
+
+
+                                //Generate a random number of milliseconds
+                                //(% 2 + 1) will result in an equal chance of
+                                //either 1 (0+1) or 2 (1+1) milliseconds
+                int ms = rand() % 2 + 1;
+                
+
+                               	//Process goes to sleep for that many milliseconds
                 sleep(ms);
 
-/*              sem_wait(mutex);
-//              ++(*table);
+                //Entering the critical section
+
+                        //Semaphore must wait if there is another process
+                        //in the critical section
+                sem_wait(mutex);
+         
+                        //Increment the array  to reflect the new position
+                        //of the first element in the stack (reflects the
+                        //consumption of the old first element of the stack) 
+                ++(*table);
+         
+         
+                        //Print statement to indicate production has occurred
+                        //and the updated status of the map (number of
+                        //elements currently)
                 printf("Item produced, there are now %d item(s) in the table.\n", *table);
+         
+         
+          
+                //Exiting the critical sectiion
+          
+                        //Updating mutex semaphore to reflect the exiting of
+                        //the critical section (wait == ++)
                 sem_post(mutex);
-*/
+
+                        //Updating the status of the number of items
+                        //in the map
                 sem_post(fill);
-/////   }
+   }
 
-/*int i = 0;
-while(sem_getvalue(fill, zero) !=0)
-{
-        sleep((rand() % 2));
-        printf("%d\n", i);
-        i++;
-}
-*/
   	//Close and unlink semaphores
-        //Deallocate shared memory
-
-//      sem_close(fill);
-/*      sem_close(avail);
+   //Deallocate shared memory
+                
+                //Close the named semaphore at the address of the given pointer 
+        sem_close(fill);
+        sem_close(avail);
         sem_close(mutex);
-        */
-//      sem_unlink("fill");
-/*      sem_unlink("available");
+ 
+               //Free the resources used to instantiate the semaphores with the given names
+        sem_unlink("fill");
+        sem_unlink("available");
         sem_unlink("mutex");
 
-        munmap(table, sizeof(int));
+               //Close the block of memory at the given address
+        close(shd_md);
 
-        close(tbl);
-        shm_unlink("table");
-*/
-  	printf("Producer2 cleaned up!"); //*/
+              //Print statement to indicate that producer clean up is finished
+  	     printf("Producer2 cleaned up!");
         return 0;
 }
